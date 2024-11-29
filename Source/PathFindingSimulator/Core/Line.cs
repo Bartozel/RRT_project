97607b82fb51daa2e59d0df99c0da2abdf5e4991
Zdenek Bartozel <Zdenek.Bartozel@bagirasys.com>
2024-11-29 15:41:01 +0100

    WPF - Add themes; UI updates

---
+++ Source/PathFindingSimulator/Core/Line.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PathFindingSimulator.Core
+{
+    internal class Line : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}

1f00be3134ec751ffa582607502d8df2809aab08
zBar <zdenek.bartozel@vtusp.cz>
2025-05-23 21:30:56 +0200

    Logging - Add Microsoft.Extensions.Logging dependencies

---
+++ Source/Logging/Helper/Extension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Logging
+{
+    public static class Extension
+    {
+        public static IServiceCollection AddCustomLogging(this IServiceCollection services)
+        {
+            var lf = LoggerFactory.Create(builder =>
+            {
+                builder.AddDebug();
+                builder.SetMinimumLevel(LogLevel.Trace);
+            });
+
+            services.AddSingleton<ILoggerFactory>(lf);
+
+            services.AddSingleton(typeof(ILogger<>), typeof(Logger<>));
+
+            return services;
+        }
+    }
+}
--- Source/Logging/ILogger.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Extensions.Logging;
-
-namespace Logging
-{
-    public interface ILogger : Microsoft.Extensions.Logging.ILogger
-    {
-        bool IsEnabled(LogLevel logLevel, string context);
-    }
-}

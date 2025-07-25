82b755976c2b11f3563da89dfb2abb57c6c8b6c4
zBar <zdenek.bartozel@vtusp.cz>
2025-07-25 15:54:00 +0200

    Simulation - Math - Add export

---
+++ Source/Simulation/Math/SpatialCalculator.cpp
@@ -0,0 +1,6 @@
+﻿#include "SpatialCalculator.h"
+
+float SpatialCalculator::Distance(const SpatialPoint& a, const SpatialPoint& b)
+{
+	return 0.0f;
+}
---
+++ Source/Simulation/Math/SpatialCalculator.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include <Data/SpatialNode.h>
+
+#define DLL_API __declspec(dllexport)
+
+class DLL_API SpatialCalculator
+{
+public:
+	static float Distance(const SpatialPoint& a, const SpatialPoint& b);
+
+private:
+	SpatialCalculator() {};
+};
+
